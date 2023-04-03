5497e096c7398169cb8ab2dc4b52b6224e7e45e2
Elizabeth Karr <e.karr93@icloud.com>
2023-04-03 10:24:31 -0500

    event listener for takeoff button and confirm popup

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@
 // Remember to pay attention to page loading!
 window.addEventListener('load', () => {
 
-    
+    const takeoffButton = document.getElementById("takeoff");
+
+    takeoffButton.addEventListener('click', () => {
+       let response = confirm("Confirm that the shuttle is ready for takeoff.");
+    });
 
 
 
