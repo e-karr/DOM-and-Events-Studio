477c9c193c7244261fd73800dc0d954e4adf2e5d
Elizabeth Karr <e.karr93@icloud.com>
2023-04-03 10:25:42 -0500

    create flightStatus variable

--- scripts.js
+++ scripts.js
@@ -3,9 +3,14 @@
 window.addEventListener('load', () => {
 
     const takeoffButton = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
 
     takeoffButton.addEventListener('click', () => {
        let response = confirm("Confirm that the shuttle is ready for takeoff.");
+
+       if (response) {
+
+       }
     });
 
 
